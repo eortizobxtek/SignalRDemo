f35061626f260e0e885f2258c174dfeb17dbb9b0
ejortiz17 <eliud.j.ortiz@gmail.com>
2020-05-06 23:51:13 -0700

    merge

---
+++ Models/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRApp.Models
+{
+    public class Instructor : Person
+    {
+
+        private string _firstName;
+        private string _lastName;
+        public int Id;
+        public string FirstName { get => _firstName; set => _firstName = value;}
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public virtual ICollection<Assignment> Assignments { get; set; }
+       
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRApp.Models
+{
+    public class Student : Person
+    {
+        public int Id;
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName { get => _firstName; set => _firstName = value;}
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public virtual ICollection<Assignment> Assignments { get; set; }
+    }
+}
