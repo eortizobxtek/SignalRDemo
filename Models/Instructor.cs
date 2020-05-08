96d0f476c8b66b47f3c053b9dccc5ab396410469
ejortiz17 <eliud.j.ortiz@gmail.com>
2020-05-08 00:13:09 -0700

    added api Controllers/AssignmentController.cs

---
+++ Models/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRApp.Models
+{
+    public class Instructor : Person
+    {
+        private string _firstName;
+        private string _lastName;
+        public string FirstName { get => _firstName; set => _firstName = value;}
+        public string LastName { get => _lastName; set => _lastName = value; }
+       
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRApp.Models
+{
+    interface Person
+    {
+        string FirstName
+        {
+            set;
+            get;
+        }
+        string LastName
+        {
+            set;
+            get;
+        }
+
+    }
+}
